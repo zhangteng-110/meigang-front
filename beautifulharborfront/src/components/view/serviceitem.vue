158acc136047332423a31090d1e217af778856ad
zhangteng-110 <910547570@qq.com>
2021-03-02 14:02:45 +0800

    初始化2.3 service item edit and delete function

--- beautifulharborfront/src/components/view/serviceitem.vue
+++ beautifulharborfront/src/components/view/serviceitem.vue
@@ -10,24 +10,43 @@ import { default } from '../home/home.vue';
                 :format="{noChecked: '${total}',hasChecked: '${checked}/${total}'}"
                 :props="{key: 'projectId',label: 'projectName'}"
                 @change="handleChange"
+                @left-check-change="handleWHLeftChange"
                 :data="data">
-                <el-button type="primary" class="transfer-footer" slot="left-footer" size="small" round @click="addProjectItem">操作</el-button>
+                <el-button type="primary" class="transfer-footer" slot="left-footer" size="small" round @click="addProjectItem">新增</el-button>
+                <el-button type="primary" class="transfer-footer" slot="left-footer" size="small" round @click="changeProjectItem">修改</el-button>
+                <el-button type="danger" class="transfer-footer" slot="left-footer" size="small" round @click="deleteProjectItem">删除</el-button>
             </el-transfer>
-        <el-dialog title="新增服务项目" width="470px" :visible.sync="dialogFormVisibleAdd" @close='cancel()' close-on-press-escape>
-            <el-form :model="serviceItem" :rules="rules" ref="addForm">
-                <el-form-item label="服务项目" prop="projectName" :label-width="formLabelWidth">
-                    <el-input clearable size="medium" v-model="serviceItem.projectName" autocomplete="off" style ="width:230px;"></el-input>
-                </el-form-item>
-                <el-form-item label="性别" :label-width="formLabelWidth">
-                    <el-radio v-model="serviceItem.sex" label="MAN">男士</el-radio>
-                    <el-radio v-model="serviceItem.sex" label="WEMAN">女士</el-radio>
-                </el-form-item>
-            </el-form>
-            <div slot="footer" class="dialog-footer" align="center">
-                <el-button @click="cancel()">取 消</el-button>
-                <el-button type="primary" @click="addServiceItem()">确 定</el-button>
-            </div>
-        </el-dialog>
+            <el-dialog title="新增服务项目" width="470px" :visible.sync="dialogFormVisibleAdd" @close='cancel()' close-on-press-escape>
+                <el-form :model="serviceItem" :rules="rules" ref="addForm">
+                    <el-form-item label="服务项目" prop="projectName" :label-width="formLabelWidth">
+                        <el-input clearable size="medium" v-model="serviceItem.projectName" autocomplete="off" style ="width:230px;"></el-input>
+                    </el-form-item>
+                    <el-form-item label="性别" :label-width="formLabelWidth">
+                        <el-radio v-model="serviceItem.sex" label="MAN">男士</el-radio>
+                        <el-radio v-model="serviceItem.sex" label="WEMAN">女士</el-radio>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer" align="center">
+                    <el-button @click="cancel()">取 消</el-button>
+                    <el-button type="primary" @click="addServiceItem()">确 定</el-button>
+                </div>
+            </el-dialog>
+
+            <el-dialog title="修改服务项目" width="470px" :visible.sync="dialogFormVisibleChange" @close='changeCancel()' close-on-press-escape>
+                <el-form :model="item" :rules="rules" ref="changeForm">
+                    <el-form-item label="服务项目" prop="projectName" :label-width="formLabelWidth">
+                        <el-input clearable size="medium" v-model="item.projectName" autocomplete="off" style ="width:230px;"></el-input>
+                    </el-form-item>
+                    <el-form-item label="性别" :label-width="formLabelWidth">
+                        <el-radio v-model="item.sex" label="MAN">男士</el-radio>
+                        <el-radio v-model="item.sex" label="WEMAN">女士</el-radio>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer" align="center">
+                    <el-button @click="changeCancel()">取 消</el-button>
+                    <el-button type="primary" @click="addServiceItem()">确 定</el-button>
+                </div>
+            </el-dialog>
         </div>
     </el-card>
 </template>
@@ -40,6 +59,11 @@ export default {
             dialogFormVisibleAdd : false,
             formLabelWidth: '120px',
             serviceItem:{},
+            dialogFormVisibleChange : false,
+            items: 0,
+            projectIds: [],
+            item: {},
+            dialogFormVisibleDelete : false,
             rules: {
                 projectName: [
                     { required: true, message: '请输入服务项目', trigger: 'blur' },
@@ -59,7 +83,6 @@ export default {
                 isClose = '1';
             }
             this.$axios.post('http://10.6.11.82:3000/meigang/service/updateStatusById',{projectIds:movedKeys,isClose:isClose}).then((result) => {
-                console.log(result.data)
                 if (result.data.errorCode == '200') {
                     this.getProjectList();
                 }else{ 
@@ -67,6 +90,7 @@ export default {
                     this.$message.error(result.data.errorMessage);
                 }
             }).catch((result) => {});
+            this.items = movedKeys
         },
         getProjectList(){
             this.$axios.get('http://10.6.11.82:3000/meigang/service/getProjectList').then((result) => {
@@ -93,7 +117,6 @@ export default {
         },
         addServiceItem(){
             this.$axios.post('http://10.6.11.82:3000/meigang/service/addServiceItem',{projectName:this.serviceItem.projectName,sex:this.serviceItem.sex}).then((result) => {
-                console.log(result.data)
                 if (result.data.errorCode == '200') {
                     this.getProjectList();
                     this.dialogFormVisibleAdd = false;
@@ -102,6 +125,48 @@ export default {
                     this.$message.error(result.data.errorMessage);
                 }
             }).catch((result) => {});
+        },
+        changeProjectItem(){
+            console.log(this.projectIds.length+"------"+this.items)
+            if (this.projectIds.length > 1 || this.projectIds.length == 0) {
+                this.$message.error("请选择一项");
+                this.projectIds = [];
+                this.items = 0;
+            }else{
+                this.dialogFormVisibleChange = true;
+                this.$axios.post('http://10.6.11.82:3000/meigang/service/getProjectById',{projectId:Number(this.items)}).then((result) => {
+                    this.item = result.data
+                }).catch((result) => {
+                    this.$message.error("请重试");
+                });
+            }
+        },
+        changeCancel(){
+            this.dialogFormVisibleChange = false;
+            this.serviceItem.projectName = '';
+            this.serviceItem.sex = '';
+            this.$refs["changeForm"].clearValidate();
+        },
+        deleteProjectItem(){
+            console.log(this.projectIds)
+            this.$axios.post('http://10.6.11.82:3000/meigang/service/deleteProjectById',{projectIds:this.projectIds}).then((result) => {
+                if (result.data.errorCode == '200') {
+                    this.getProjectList();
+                }else{ 
+                    this.getProjectList();
+                    this.$message.error(result.data.errorMessage);
+                }
+            }).catch((result) => {});
+        }
+        ,handleWHLeftChange(value, direction){
+            console.log(value)
+            if (value.length > 1) {
+                this.projectIds = value;
+                console.log(this.projectIds+"*****")
+            } else {
+                this.items = value;
+                this.projectIds = value;
+            }
         }
     }
 }
